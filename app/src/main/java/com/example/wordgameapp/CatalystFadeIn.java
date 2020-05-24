095aa5748762e96cc9d395b201de70417b836ec1
John Aron <johnaronbelmonte@gmail.com>
2020-05-24 22:27:54 +0800

    -Added music player -Added fade animations -Started not taking the project seriously

---
+++ app/src/main/java/com/example/wordgameapp/CatalystFadeIn.java
@@ -0,0 +1,17 @@
+package com.example.wordgameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class CatalystFadeIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalyst_fade_in);
+        Intent intent = new Intent(CatalystFadeIn.this, SplashScreen.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/wordgameapp/SplashScreen.java
+++ app/src/main/java/com/example/wordgameapp/SplashScreen.java
@@ -3,6 +3,7 @@ package com.example.wordgameapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -11,13 +12,18 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        MediaPlayer musicPlayer = MediaPlayer.create(this, R.raw.terrariajourneysendrelogic1);
+        musicPlayer.start();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                 finish();
             }
-        }, 4000);
+        }, 9000);
     }
 }
