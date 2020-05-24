97a3f190ad0fc20dbb000c788853836ea085d778
Frankie <2015079670@ust-ics.mygbiz.com>
2020-05-24 23:58:27 +0800

    checkout revision test 2 to aron

--- app/src/main/java/com/example/wordgameapp/AboutUs.java
+++ app/src/main/java/com/example/wordgameapp/AboutUs.java
@@ -12,6 +12,7 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        //test 2 
     }
 
     public void jumpToMainMenu (View v){
